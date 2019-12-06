2cf33f591b4bdd8c53746105463cc92dc201d82b
yesha.ailani <yesha.ailani28@gmail.com>
2019-12-06 01:29:36 -0800

    Initial Commit

---
+++ ThoughtStack/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ThoughtStack
+//
+//  Created by Yesha Ailani on 12/6/19.
+//  Copyright © 2019 Yesha Ailani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
